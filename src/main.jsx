34734bf08d4f98628cf77b4a74714bcce3c71105
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-01-22 22:23:35 +0600

    update project layout & config firebase , added font

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <h2 className='text-3xl font-bold text-red-500 font-lato'>Rana</h2>
+  </StrictMode>,
+)
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: "'Lato', serif",
+        inter: "'Inter', serif"
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
